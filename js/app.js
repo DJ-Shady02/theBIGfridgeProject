1edbfb837a41b0846296015a0ca105ec9fdb9960
martilk <m.l.kjeldsen@gmail.com>
2019-12-16 12:59:35 +0100

    HTML Templates
    Update with html templates before removing ionic.

--- js/app.js
+++ js/app.js
@@ -16,9 +16,26 @@ function presentModal() {
   return modalElement.present();
 }
 
+customElements.define('opslag-modal-page', class extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = document.getElementById('modal-opslag').innerHTML;
+  }
+});
+
+function presentOpslagModal() {
+  // create the modal with the `modal-page` component
+  const modalElement = document.createElement('ion-modal');
+  modalElement.component = 'opslag-modal-page';
+  // present the modal
+  document.body.appendChild(modalElement);
+  return modalElement.present();
+}
+
 async function dismissModal() {
   let modal = document.getElementsByTagName('ion-modal')[0];
   await modal.dismiss({
     'dismissed': true
   });
 }
+
+// Swipe function //
