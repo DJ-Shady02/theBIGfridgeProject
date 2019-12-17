ca8ff471d6d70e3b0a2a8fe8d5222fe6b4f1a29f
martilk <m.l.kjeldsen@gmail.com>
2019-12-17 16:56:18 +0100

    Modal update
    modal css / javascript og styling

--- js/app.js
+++ js/app.js
@@ -39,3 +39,21 @@ async function dismissModal() {
   });
 }
 */
+
+//-------Modal function ------//
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+function openModal() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
